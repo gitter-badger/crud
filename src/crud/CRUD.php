2507250182db4ddeb35b5b341ac791e21d8589d9
blackfyre <gnick666@gmail.com>
2015-03-30 22:43:14 +0200

    started building

---
+++ src/crud/CRUD.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Meki
+ * Date: 2015.03.30.
+ * Time: 22:40
+ */
+
+namespace BlackfyreSudio\CRUD;
+
+
+class CRUD {
+    /**
+     * Laravel application
+     *
+     * @var \Illuminate\Foundation\Application
+     */
+    public $app;
+
+    /**
+     * Create a new confide instance.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     *
+     * @return void
+     */
+    public function __construct($app)
+    {
+        $this->app = $app;
+    }
+}
