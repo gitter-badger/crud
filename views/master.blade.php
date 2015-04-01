3ac875e8bf352d69dd9c9197a48cddacdcc3d1c8
blackfyre <gnick666@gmail.com>
2015-04-01 06:30:29 +0200

    updated master blade added morris sample data

--- views/master.blade.php
+++ views/master.blade.php
@@ -36,7 +36,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.html">SB Admin v2.0</a>
+            <a class="navbar-brand" href="{{URL::route('crud.home')}}">{{Config::get('crud-config.title')}}</a>
         </div>
         <!-- /.navbar-header -->
 
@@ -265,7 +265,7 @@
                         <!-- /input-group -->
                     </li>
                     <li>
-                        <a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                        <a href="{{URL::route('crud.home')}}"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
                     </li>
                     <li>
                         <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Charts<span class="fa arrow"></span></a>
