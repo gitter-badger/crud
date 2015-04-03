0ec14716b000d0bcc1c482e31750c98a30e6ac94
Galicz Miklós <gnick666@gmail.com>
2015-04-03 15:59:28 +0200

    scaffold command before scrapping

--- src/Console/ScaffoldCommand.php
+++ src/Console/ScaffoldCommand.php
@@ -9,6 +9,8 @@
 namespace BlackfyreStudio\CRUD\Console;
 
 use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
 
 class ScaffoldCommand extends Command {
     /**
@@ -25,7 +27,45 @@ class ScaffoldCommand extends Command {
      */
     protected $description = 'Generate a new set of Models, Controllers and Migrations';
 
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
     public function __construct() {
         parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function fire()
+    {
+        //
+
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [
+            ['name', InputArgument::REQUIRED, 'The name of the module you wish to create'],
+        ];
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [];
     }
 }
