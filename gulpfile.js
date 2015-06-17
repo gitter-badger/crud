ad5c039033a742b3eaf980a28844d9e6b70c120e
blackfyre <gnick666@gmail.com>
2015-06-17 07:00:24 +0200

    Cleaned up the admin template
    The admin template is now a bower dependency of the admin panel

--- gulpfile.js
+++ gulpfile.js
@@ -1,76 +1,29 @@
-var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    jshint = require('gulp-jshint'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename');
+var gulp = require('gulp');
+
+var bowerBase = 'resources/bower/startbootstrap-sb-admin-2-sass/dist'
 
 
-var paths = {
-    "sass": "resources/sass/",
-    "bower": "resources/bower/",
-    "vendorJs": [
-        "resources/bower/jquery/dist/jquery.js",
-        "resources/bower/bootstrap-sass-official/assets/javascripts/bootstrap.js",
-        "resources/bower/metisMenu/src/metisMenu.js",
-        "resources/bower/raphael/raphael.js",
-        "resources/bower/morrisjs/morris.js"
-    ],
-    "js": "resources/js/",
-    "fonts": [
-        "resources/bower/bootstrap-sass-official/assets/fonts/bootstrap/*.*",
-        "resources/bower/font-awesome/fonts/*.*"
-    ]
-};
-
-gulp.task('sass', function () {
-    return sass([
-        paths.sass + "style.scss"
-    ], {sourcemap: true})
-        .on('error', function (err) {
-            console.error('Error!', err.message);
-        })
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('public/styles'));
+gulp.task('styles:copy', function() {
+  return gulp.src( bowerBase + '/css/*.css')
+      .pipe(gulp.dest('public/styles'))
 });
 
-gulp.task('lint', function () {
-    return gulp.src(paths.js + '*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
+gulp.task('scripts:copy', function() {
+  return gulp.src(bowerBase + '/js/*.js')
+      .pipe(gulp.dest('public/scripts'));
 });
 
-gulp.task('scripts', function () {
-    var js = paths.vendorJs;
-    js.push(paths.js + '*.js');
-
-    return gulp.src(js)
-        .on('error', function (err) {
-            console.error('Error!', err.message);
-        })
-        .pipe(concat('main.js'))
-        .pipe(gulp.dest('public/scripts'))
-        .pipe(rename('main.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('public/scripts'));
+gulp.task('fonts:copy', function() {
+  return gulp.src(bowerBase + '/fonts/*')
+      .pipe(gulp.dest('public/fonts'));
 });
 
-gulp.task('fonts', function () {
-    return gulp.src(paths.fonts)
-        .on('error', function (err) {
-            console.error('Error!', err.message);
-        })
-        .pipe(gulp.dest('public/fonts'));
+gulp.task('images:copy', function () {
+  return gulp.src(bowerBase + '/images/*')
+      .pipe(gulp.dest('public/images'));
 });
 
-gulp.task('watch', function () {
 
-    gulp.watch(paths.sass + "**/*.scss", ['sass']);
-    gulp.watch(paths.js + '*.js', ['lint', 'scripts']);
-
-});
-
-gulp.task('default', function () {
+gulp.task('default', ['styles:copy', 'scripts:copy', 'fonts:copy','images:copy'], function () {
     // place code for your default task here
 });
